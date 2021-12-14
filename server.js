22c43f3d6b964a034e1fad7753d0412fce51a3f8
Ashley <ashleyviola@Ashleys-MacBook-Pro.local>
2021-12-13 21:14:44 -0500

    connected to database

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 // imports express 
 const express = require('express');
+const database = require('mime-db');
+
+//imports mysql2
+const mysql = require('mysql2');
 
 //port designation and app expression
 const PORT = process.env.PORR || 3001;
@@ -13,6 +17,19 @@ app.use(express.json());
 app.use((req,res) => {
     res.status(404).end();
 });
+
+// connect to database 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your MySQL username,
+        user: 'root',
+        //Your MySQL password 
+        password: 'Ishley14',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 // starts express.js
 app.listen(PORT, () => {
