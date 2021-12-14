d847890eab5956efa87f6eb9baf66e5ebddec9fe
Ashley <ashleyviola@Ashleys-MacBook-Pro.local>
2021-12-13 21:32:14 -0500

    added ability to read, delete and add candidates

--- server.js
+++ server.js
@@ -25,11 +25,38 @@ const db = mysql.createConnection(
     },
     console.log('Connected to the election database.')
 );
+// gets information from candiate table rows
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
+// read operation 
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err,row) => {
+    if(err){
+        console.log(err);
+    }
+    console.log(row);
 });
 
+// delete operation 
+//? denotes a placeholder 
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) =>{
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// create candidate 
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+                VALUES(?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+db.query(sql, params, (err,result) => {
+    if(err){
+        console.log(err);
+    }
+    console.log(result);
+});
 // default response for any other request (not found)
 app.use((req,res) => {
     res.status(404).end();
